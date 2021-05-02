4fa0673e78d6b726c3b74e61e35273c59b0f0438
Sumit-singh18 <ss7262692@gmail.com>
2021-05-02 11:33:25 +0530

    -A

---
+++ promise.js
@@ -0,0 +1,25 @@
+// promises
+// 1. inside catch =reject
+// 2.inside then= rsolve
+// 3.pending
+
+function func1() {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            const error = true;
+            if (!error) {
+                console.log('function: your function is resolved')
+                resolve();
+            } else {
+                console.log('function: your function is not resolved');
+                reject();
+            }
+
+        }, 2000);
+    })
+}
+func1().then(function() {
+    console.log('me:thanks for resolve');
+}).catch(function(error) {
+    console.log('me: very bad performance');
+})

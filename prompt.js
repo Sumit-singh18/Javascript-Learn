1eeeffc1383b4fa541c23a3eb7fd1db200c02569
Sumit-singh18 <ss7262692@gmail.com>
2022-03-19 11:59:03 +0530

    -a

--- prompt.js
+++ prompt.js
@@ -8,3 +8,12 @@ else if(year>2000){
 else{
     alert("go to hell")
 }
+
+
+
+
+let username = prompt('your name ?', 'Sumit'); //setting a default value sumit in question
+let Ask = confirm('do u want tea?')
+
+alert(`visitor: ${username}`)
+alert(`they want tea ${Ask}`)

378e94481461fe19cb5db7a9a667f1096a36f2f9
Sumit-singh18 <ss7262692@gmail.com>
2022-03-22 17:17:00 +0530

    -A

---
+++ objectt.js
@@ -0,0 +1,58 @@
+for (let i = 0; i < 50; i++) {
+    // alert(i)
+}
+// let arr = [4, 5, 6, 7, 6, 4];
+// arr.forEach(function(element) {
+//     alert(element);
+// });
+
+
+// let ojec = {
+//     name: "sumit",
+//     age: 45,
+//     heyee: 656,
+//     os: "ubuntu"
+// }
+// for (let key in ojec) {
+//     alert(`the ${key} of object is ${ojec[key]}`)
+// }
+
+//  functiom in javascript
+
+// function wish(name , thanks ) {
+//     alert(`happy birthday ${name} You have been ${thanks} there for me no matter what. I love you, my dear friend, and I am so excited to share your special day with you. Your birthday is going to be truly special.`)
+// }
+// let naame = "sumit";
+// let thanks = "snehal";
+// let wish2 =  'snehalllll '
+
+// first wAY 
+//  wish(`sumitt`,`snehal`)
+
+// second way 
+// wish(naame , wish2);
+
+// // M<ake function in object 
+// let myobj = {
+//     name: "sumit",
+//     game: function() {
+//         return "BGMI";
+
+//     }
+// }
+// alert(myobj.game())
+// alert(myobj.name);
+
+
+// Promt
+// b = prompt("please fill your full name");
+// alert(b);
+
+// confirm
+// c = confirm(" are you afraid");
+// alert(c);
+
+// d = innerHeight;
+// d = scrollX;
+// d = location
+// console.log(d);

9ae63da0d2d1098d6f4b5cc344eff84baa7fa797
Sumit-singh18 <ss7262692@gmail.com>
2022-03-28 01:01:26 +0530

    A

--- constructrs.js
+++ constructrs.js
@@ -7,10 +7,10 @@ let car = {
     name: "Tiago",
     Topspeed: 99,
     run: function() {
-        console.log("car is running");
+        // console.log("car is running");
     },
 };
-console.log(car);
+// console.log(car);
 
 // create a constructor for cars 
 function GeneralCar(givename, givenspeed, givenmodel) {
@@ -28,3 +28,19 @@ function GeneralCar(givename, givenspeed, givenmodel) {
 car1 = new GeneralCar("nissan", "56", "2019");
 car2 = new GeneralCar("breeza", "78", "2012");
 console.log(car2);
+
+
+// another consructor
+function Partydetails(Partyname,Partyid, age){
+    this.name= Partyname;
+    this.id = Partyid;
+    if(age>18){
+        console.log(`${this.name} is allowed in party with his party id ${this.id}`)
+    }
+ else{
+     console.log(`${this.name} is underage`)
+ }
+};
+entry = new Partydetails("Sumit", 17, 21);
+entry1= new Partydetails ("Snehal", 16, 11);
+console.log(entry)

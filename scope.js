b53e943defbdab7a350f8d1c0f34aa7fc2ffc88b
Sumit-singh18 <ss7262692@gmail.com>
2022-05-11 12:18:49 +0530

    A

--- scope.js
+++ scope.js
@@ -1,7 +1,26 @@
 
-const Myfunc=()=>{
-     a = 'ascope chain';
-     console.log(a)
+// const Myfunc=()=>{
+//      a = 'ascope chain';
+//      console.log(a)
 
+// }
+// Myfunc()
+
+{
+      let a =12;
+      const b= 4;
+      var c =9;
+
+console.log(a)
+console.log(b)
+console.log(c)
 }
-Myfunc()
+
+console.log(c)
+console.log(b)
+console.log(a)
+ 
+
+
+
+
---
+++ undefvsdef.js
@@ -0,0 +1,7 @@
+// undefined vs not defined.
+console.log(a);     //this is undefined becasue we are accessing the element before intotlize / before declare the varible with some variable(var a;)
+var a =3;
+console.log(a)
+console.log(c)
+
+

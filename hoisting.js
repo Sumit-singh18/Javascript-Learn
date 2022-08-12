8f60b3fc01712167998d79fac2571e4cb1da94d4
Sumit-singh18 <ss7262692@gmail.com>
2022-08-12 09:19:02 +0530

    -A

--- hoisting.js
+++ hoisting.js
@@ -8,20 +8,25 @@
 // temporal deadzone - It means when the variable is assigned a memory but value of that variable is not Initilize then the time b/w that is known as
 // temporal deadzone
 
-function codeHoist(){   
- a = 11;   
-  b = 50;   
-  a+b;
-}   
-codeHoist();  
+// function codeHoist(){   
+//  a = 11;   
+//  let b = 50;   
+//   a+b;
+// }   
+// codeHoist();  
 
 
-function fun(){  
-console.log(a); // 11  
-console.log(b); // ReferenceError : b is not defined  
+// function fun(){  
+// console.log(a); // 11  
+// console.log(b); // ReferenceError : b is not defined  
 
-} 
-fun();
+// } 
+// fun();
+console.log(a)
+var a = 10;
+let y =100;
+
+
 // let and const are also hoisted but in temporal deadzone
 
 
@@ -30,5 +35,5 @@ fun();
 // This happens because the scope of the variable 'a' is changed to global by the JavaScript 
 // itself (or we can say variable a is converted to a global variable) that'swhy the function "fun();"
 // but it will not print the value of the variable that we declared using the let keyword. 
-//  isable to print the value of the variable. But the function "fun();" isnot able to print the value of variable b because of declaring it using 
+//  is able to print the value of the variable. But the function "fun();" isnot able to print the value of variable b because of declaring it using 
 //  the "let" keyword due to which its scope gets confined to function definition, and it is not available for any outsider function

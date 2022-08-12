0d0c721458d1b8bce1d6188ce5d77b6617bad36f
Sumit-singh18 <ss7262692@gmail.com>
2022-08-12 09:55:51 +0530

    -A

--- Interview Practice/palandrome.js
+++ Interview Practice/palandrome.js
@@ -1,10 +1,10 @@
-let myvalue = 'mAdam';
+let myvalue = 121;
 // let myvalue = 121;
 
 const CheckValue=()=>{
-     let uppervalue= myvalue.toUpperCase();
-    let RevVal = uppervalue.toString().split('').reverse().join('')
-    let StringVal = uppervalue.toString();
+    //  let uppervalue= myvalue.toUpperCase();
+    let RevVal = myvalue.toString().split('').reverse().join('')
+    let StringVal = myvalue.toString();
     if(RevVal==StringVal){
         console.log('its a palandrome')
     }
